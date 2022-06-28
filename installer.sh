7eef7999f7283f80cae9bec7ebd6a44c9fedfbc1
RAED <rrrr53@hotmail.com>
2022-06-28 23:59:53 +0300

    [installer.sh] Update

--- installer.sh
+++ installer.sh
@@ -23,7 +23,7 @@ else
    STATUS=/var/lib/opkg/status
    OSTYPE=Dream
 fi
-
+echo ""
 if python --version 2>&1 | grep -q '^Python 3\.'; then
 	echo "You have Python3 image"
 	PYTHON=PY3
@@ -66,18 +66,18 @@ fi
 echo ""
 ## Remove old file from tmp directory
 [ -d $BACKUPPATH ] && rm -rf $BACKUPPATH
-[ -d $PLUGINPICONTMPPATH ] && rm -rf $PLUGINPICONTMPPATH
-[ -r /tmp/RaedQuickSignal-"$version".tar.gz ] && rm -f /tmp/RaedQuickSignal-"$version".tar.gz
-[ -r /tmp/RaedQuickServName2-dreamos.tar.gz ] && rm -f /tmp/RaedQuickServName2-dreamos.tar.gz
+[ -d $PLUGINPICONTMPPATH ] && rm -rf $PLUGINPICONTMPPATH > /dev/null 2>&1
+[ -r /tmp/RaedQuickSignal-"$version".tar.gz ] && rm -f /tmp/RaedQuickSignal-"$version".tar.gz > /dev/null 2>&1
+[ -r /tmp/RaedQuickServName2-dreamos.tar.gz ] && rm -f /tmp/RaedQuickServName2-dreamos.tar.gz > /dev/null 2>&1
 ### backup Current files/Folders and save it beffore delete old version of plugin
 mkdir -p $BACKUPPATH
-[ -r $PLUGINPATH/tools/keymap.xml ] && cp -f $PLUGINPATH/tools/keymap.xml $BACKUPPATH/keymap.xml
+[ -r $PLUGINPATH/tools/keymap.xml ] && cp -f $PLUGINPATH/tools/keymap.xml $BACKUPPATH/keymap.xml > /dev/null 2>&1
 
-[ -d $PLUGINPATH/PICONS/emu ] && cp -r $PLUGINPATH/PICONS/emu $BACKUPPATH
-[ -d $PLUGINPATH/PICONS/piconSat ] && cp -r $PLUGINPATH/PICONS/piconSat $BACKUPPATH
-[ -d $PLUGINPATH/PICONS/piconProv ] && cp -r $PLUGINPATH/PICONS/piconProv $BACKUPPATH
-[ -d $PLUGINPATH/PICONS/piconCrypt ] && cp -r $PLUGINPATH/PICONS/piconCrypt $BACKUPPATH
-[ -d $PLUGINPATH/PICONS/weather ] && cp -r $PLUGINPATH/PICONS/weather $BACKUPPATH
+[ -d $PLUGINPATH/PICONS/emu ] && cp -r $PLUGINPATH/PICONS/emu $BACKUPPATH > /dev/null 2>&1
+[ -d $PLUGINPATH/PICONS/piconSat ] && cp -r $PLUGINPATH/PICONS/piconSat $BACKUPPATH > /dev/null 2>&1
+[ -d $PLUGINPATH/PICONS/piconProv ] && cp -r $PLUGINPATH/PICONS/piconProv $BACKUPPATH > /dev/null 2>&1
+[ -d $PLUGINPATH/PICONS/piconCrypt ] && cp -r $PLUGINPATH/PICONS/piconCrypt $BACKUPPATH > /dev/null 2>&1
+[ -d $PLUGINPATH/PICONS/weather ] && cp -r $PLUGINPATH/PICONS/weather $BACKUPPATH > /dev/null 2>&1
 
 [ -r $PLUGINPATH ] && rm -rf $PLUGINPATH
 # Download and install plugin + converter files
@@ -115,42 +115,42 @@ sleep 2
 
 mkdir -p $PLUGINPATH/PICONS
 ### move backup files/Folders save to plugin
-[ -r $BACKUPPATH/keymap.xml ] && mv $BACKUPPATH/keymap.xml $PLUGINPATH/tools
-[ -d $BACKUPPATH/emu ] && mv $BACKUPPATH/emu $PLUGINPATH/PICONS
-[ -d $BACKUPPATH/piconSat ] && mv $BACKUPPATH/piconSat $PLUGINPATH/PICONS
-[ -d $BACKUPPATH/piconProv ] && mv $BACKUPPATH/piconProv $PLUGINPATH/PICONS
-[ -d $BACKUPPATH/piconCrypt ] && mv $BACKUPPATH/piconCrypt $PLUGINPATH/PICONS
-[ -d $BACKUPPATH/weather ] && mv $BACKUPPATH/weather $PLUGINPATH/PICONS
+[ -r $BACKUPPATH/keymap.xml ] && mv $BACKUPPATH/keymap.xml $PLUGINPATH/tools > /dev/null 2>&1
+[ -d $BACKUPPATH/emu ] && mv $BACKUPPATH/emu $PLUGINPATH/PICONS > /dev/null 2>&1
+[ -d $BACKUPPATH/piconSat ] && mv $BACKUPPATH/piconSat $PLUGINPATH/PICONS > /dev/null 2>&1
+[ -d $BACKUPPATH/piconProv ] && mv $BACKUPPATH/piconProv $PLUGINPATH/PICONS > /dev/null 2>&1
+[ -d $BACKUPPATH/piconCrypt ] && mv $BACKUPPATH/piconCrypt $PLUGINPATH/PICONS > /dev/null 2>&1
+[ -d $BACKUPPATH/weather ] && mv $BACKUPPATH/weather $PLUGINPATH/PICONS > /dev/null 2>&1
 ### Checking if picons folder avinable on plugin
 if [ ! -d $PLUGINPATH/PICONS/emu ]; then
 	echo "Missing emu folder will be send it to plugin path"
-	cp -r $PLUGINPICONTMPPATH/emu $PLUGINPATH/PICONS
+	cp -r $PLUGINPICONTMPPATH/emu $PLUGINPATH/PICONS > /dev/null 2>&1
 else
-	cp -u $PLUGINPICONTMPPATH/emu/* $PLUGINPATH/PICONS/emu
+	cp -u $PLUGINPICONTMPPATH/emu/* $PLUGINPATH/PICONS/emu > /dev/null 2>&1
 fi
 if [ ! -d $PLUGINPATH/PICONS/piconProv ]; then
 	echo "Missing piconProv folder will be send it to plugin path"
-	cp -r $PLUGINPICONTMPPATH/piconProv $PLUGINPATH/PICONS
+	cp -r $PLUGINPICONTMPPATH/piconProv $PLUGINPATH/PICONS > /dev/null 2>&1
 else
-	cp -u $PLUGINPICONTMPPATH/piconProv/* $PLUGINPATH/PICONS/piconProv
+	cp -u $PLUGINPICONTMPPATH/piconProv/* $PLUGINPATH/PICONS/piconProv > /dev/null 2>&1
 fi
 if [ ! -d $PLUGINPATH/PICONS/piconSat ]; then
 	echo "Missing piconSat folder will be send it to plugin path"
-	cp -r $PLUGINPICONTMPPATH/piconSat $PLUGINPATH/PICONS
+	cp -r $PLUGINPICONTMPPATH/piconSat $PLUGINPATH/PICONS > /dev/null 2>&1
 else
-	cp -u $PLUGINPICONTMPPATH/piconSat/* $PLUGINPATH/PICONS/piconSat
+	cp -u $PLUGINPICONTMPPATH/piconSat/* $PLUGINPATH/PICONS/piconSat > /dev/null 2>&1
 fi
 if [ ! -d $PLUGINPATH/PICONS/piconCrypt ]; then
 	echo "Missing piconCrypt folder will be send it to plugin path"
-	cp -r $PLUGINPICONTMPPATH/piconCrypt $PLUGINPATH/PICONS
+	cp -r $PLUGINPICONTMPPATH/piconCrypt $PLUGINPATH/PICONS > /dev/null 2>&1
 else
-	cp -u $PLUGINPICONTMPPATH/piconCrypt/* $PLUGINPATH/PICONS/piconCrypt
+	cp -u $PLUGINPICONTMPPATH/piconCrypt/* $PLUGINPATH/PICONS/piconCrypt > /dev/null 2>&1
 fi
 if [ ! -d $PLUGINPATH/PICONS/weather ]; then
 	echo "Missing weather folder will be send it to plugin path"
-	cp -r $PLUGINPICONTMPPATH/weather $PLUGINPATH/PICONS
+	cp -r $PLUGINPICONTMPPATH/weather $PLUGINPATH/PICONS > /dev/null 2>&1
 else
-	cp -u $PLUGINPICONTMPPATH/weather/* $PLUGINPATH/PICONS/weather
+	cp -u $PLUGINPICONTMPPATH/weather/* $PLUGINPATH/PICONS/weather > /dev/null 2>&1
 fi
 rm -rf $PLUGINPICONTMPPATH
 rm -rf $BACKUPPATH
