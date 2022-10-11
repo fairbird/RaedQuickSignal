b1364f70d73de8f980b3351d71a10de4c3457d6e
RAED <rrrr53@hotmail.com>
2022-10-11 18:59:29 +0300

    Bump V14.9

--- installer.sh
+++ installer.sh
@@ -2,8 +2,8 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/fairbird/RaedQuickSignal/main/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=14.8
-description=What is NEW:\n-Fix url download line fir picons\n\n*************************\n:ما هو الجديد\n-تعديل رابط تحميل البايكونات
+version=14.9
+description=What is NEW:\n-Fix add city in weather list\n\n*************************\n:ما هو الجديد\n-تعديل أدخال إسم المدنية لحالة الطقس
 ##############################################################
 
 BACKUPPATH=/tmp/Backup
