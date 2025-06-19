ddb29cd644c0ea4f96f49f49c206f6d81a0ccaad
RAED <rrrr53@hotmail.com>
2025-06-19 12:31:25 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,8 +2,8 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/fairbird/RaedQuickSignal/main/installer.sh -O - | /bin/sh
 
 ######### Only These two lines to edit with new version ######
-version=17.7
-description=What is NEW:\n- Add new option in menu button \n\n*************************\n:ما هو الجديد\n- menu إضافة خاصية جديدة في زر
+version=17.8
+description=What is NEW:\n- Add Russian language support\n-Remove the option to select the city name for the weather condition from the cities list (this option is no longer useful) and only rely on the manual option  \n\n*************************\n:ما هو الجديد\n-حذف خيار اختيار أسم المدينة لحالة الطقس من خلال قائمة المدن (لم يعد ينفع هذا الخيار) فقط الإعتماد على الخيار اليدوي \n-إضافة دعم اللغة الروسية
 ##############################################################
 
 BACKUPPATH=/tmp/Backup
